41ede2bfb82c14d00276d5d57d18b37efe0a7a20
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2023-03-13 16:19:53 -0300

    [aula 3|desafio 1] usando record

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -8,13 +8,11 @@ public class App {
 
         // fazer uma conexão HTTP e buscar os top 250 filmes
 
-        // String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
-        // String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        // ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_x3pev8lm";
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
 
-        // String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/NASA-APOD.json";
-        ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+        //String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
+        //ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
 
         var http = new ClienteHttp();
         String json = http.buscaDados(url);
@@ -28,12 +26,12 @@ public class App {
 
             Conteudo conteudo = conteudos.get(i);
 
-            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
-            String nomeArquivo = "saida/" + conteudo.getTitulo() + ".png";
+            InputStream inputStream = new URL(conteudo.urlImagem()).openStream();
+            String nomeArquivo = "saida/" + conteudo.titulo() + ".png";
 
             geradora.cria(inputStream, nomeArquivo);
 
-            System.out.println(conteudo.getTitulo());
+            System.out.println(conteudo.titulo());
             System.out.println();
         }
     }
