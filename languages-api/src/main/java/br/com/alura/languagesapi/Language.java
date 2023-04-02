356142c72a55eb60231a6764a59cabede4dfb21b
Tuanemendes <tuane.belmiro@gmail.com>
2023-04-02 16:54:36 -0300

    Revert "busca de dados no mongo"
    This reverts commit afd8d5d40240da4b6e1a8d99e8968ee9b5732a19.

--- languages-api/src/main/java/br/com/alura/languagesapi/Language.java
+++ languages-api/src/main/java/br/com/alura/languagesapi/Language.java
@@ -1,29 +1,17 @@
 package br.com.alura.languagesapi;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "languagesPrincipal")
 public class Language {
     
-    @Id
-    private String id;
     private String title;
     private String image;
     private int ranking;
 
-    public Language(){
-        
-    }
     
     public Language(String title, String image, int ranking) {
         this.title = title;
         this.image = image;
         this.ranking = ranking;
-    }
-
-    public String getId() {
-        return id;
     }
     public String getTitle() {
         return title;
@@ -34,7 +22,6 @@ public class Language {
     public int getRanking() {
         return ranking;
     }
-
 
     
     
