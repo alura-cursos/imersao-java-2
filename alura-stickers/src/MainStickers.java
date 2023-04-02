1ca9f43b1ffae840b518028b0d415ccdc22569ee
Tuanemendes <tuane.belmiro@gmail.com>
2023-04-02 02:29:31 -0300

    criação do Enum API

--- alura-stickers/src/MainStickers.java
+++ alura-stickers/src/MainStickers.java
@@ -12,12 +12,13 @@ public class MainStickers {
         ClientHttp clientHttp = new ClientHttp();
         String json = clientHttp.getlistDados();
         
-        //manipula os dados 
-        ContentExtractorNASA extractorNASA = new ContentExtractorNASA();
-        List<Content> contents = extractorNASA.getContent(json);
+       
+        ContentExtractorNASA extractor = new ContentExtractorNASA();
+        
         //ContentExtractorIMDB extractorIMDB = new ContentExtractorIMDB();
-        //List<Content> contents = extractorIMDB .getContent(json);
 
+        //manipula os dados 
+        List<Content> contents = extractor.getContent(json);
         var generatImage = new StickerGenerator();
         
         //criação do diretorio
