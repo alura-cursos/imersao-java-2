ba63ac2d251dc5ed68cc1c2b0d546a783107681d
Tuanemendes <tuane.belmiro@gmail.com>
2023-04-02 16:54:36 -0300

    Revert "busca de dados no mongo"
    This reverts commit afd8d5d40240da4b6e1a8d99e8968ee9b5732a19.

--- languages-api/src/main/java/br/com/alura/languagesapi/LanguageController.java
+++ languages-api/src/main/java/br/com/alura/languagesapi/LanguageController.java
@@ -2,17 +2,18 @@ package br.com.alura.languagesapi;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LanguageController {
     
-    @Autowired
-    private LanguageRepository languageRepository;
+
+    private List<Language> languages=
+        List.of(
+            new Language("Java", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_512x512.png", 1),
+            new Language("JavaScript", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/javascript/javascript_512x512.png", 2)
+        );
        
     @GetMapping(value = "/language")
     public String getLanguage(){
@@ -21,13 +22,7 @@ public class LanguageController {
 
     @GetMapping(value = "/languages")
     public List<Language> getlanguageList(){
-        List<Language> languages = languageRepository.findAll();
         return languages;
-    }
-
-    @PostMapping(value = "/languages")
-    public Language addLanguage(@RequestBody  Language language){
-        return languageRepository.save(language);
     }
 }
 
