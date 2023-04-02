941f9d4abf89b9a77bc5d5e538a36402e6105530
Tuanemendes <tuane.belmiro@gmail.com>
2023-04-02 00:54:30 -0300

    refatoraçção com a utilização do lambda e stream

--- alura-stickers/src/ContentExtractorNASA.java
+++ alura-stickers/src/ContentExtractorNASA.java
@@ -10,17 +10,9 @@ public class ContentExtractorNASA {
         var jsonParser = new JsonParser();
         List<Map<String, String>> attributeList = jsonParser.parse(json);
 
-        List<Content> contentList = new ArrayList<>();
-
         //popular a lista de conteudos 
-        for (Map<String, String> attributes : attributeList) {
-            String title = attributes.get("title");
-            String urlImage = attributes.get("url");
-            var content = new Content(title, urlImage);
-
-            contentList.add(content);
-        }
-        
-        return contentList;
+         return attributeList.stream()
+            .map(attributes ->  new Content(attributes.get("title"),attributes.get("url")))
+            .toList();   
     }
 }
