2b69b26c5e0082ab488f35b380dff1bdf795b64e
Tuanemendes <tuane.belmiro@gmail.com>
2023-04-03 21:54:33 -0300

    deploy da api languages

--- alura-stickers/src/API.java
+++ alura-stickers/src/API.java
@@ -2,7 +2,7 @@ public enum API {
     IMDB_TOP_MOVIES("https://imdb-api.com/en/API/Top250Movies/",new ContentExtractorIMDB()),
     IMDB_MOST_POPULAR("https://imdb-api.com/en/API/MostPopularMovies/",new ContentExtractorIMDB()),
     NASA_APOD("https://api.nasa.gov/planetary/apod?api_key=",new ContentExtractorNASA()),
-    LANGUAGE("http://localhost:8080/languages", new ContentExtractorLanguage());
+    LANGUAGE("https://language-api-alura.fly.dev/languages", new ContentExtractorLanguage());
     private String url;
     private ContentExtractor extractor;
 
